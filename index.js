69c004beb1d1a014b4fdef54ed6e5eb1b8687e7f
Harmony <harmonymukolwe@gmail.com>
2024-06-21 13:59:14 +0300

    Writes functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello(){
+  console.log("Hello!")
+}
+
+sayHello()
+
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!")
+}
+
+sayHelloToGuadalupe()
+
+function add(x,y) {
+  return x+y;
+}
+
+console.log(add(1,2))
+
+function say(greeting, firstName) {
+  console.log("I was called!");
+  return `${greeting}, ${firstName}!`;
+}
